6944066a80689af9ab39c16946a29f3c916e0d2c
Sophia <sophieyawson12@gmail.com>
2024-05-19 19:34:25 +0000

    a  JavaScript function named formatArrayStrings that takes two arrays as arguments An array of stringsand An array of numbers processed by processArray.

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -17,4 +17,35 @@ for(let i=0; i<numbers.length; i++){
 
 }
 
+//Task TWO(2)
+function formatArrayStrings(strings,numbers) {
+    for (let i = 0; i < strings.length; i++) {
+        if (numbers[i] % 2 === 0) {
+            
+            strings[i] = strings[i].toUpperCase();
+        } else {
+           
+            strings[i] = strings[i].toLowerCase();
+        }
+    }
+    return strings;
+}
+
+const testOne = ["nhyira", "yooku", "agape", "cornelius", "evelyn", "catherine"];
+const testTwo = [1, 2, 3, 4, 5, 6];
+
+// To modify the names for test0ne;
+const modifiedNames = formatArrayStrings([...testOne], testTwo); // Spread test into a new array
+
+if (require.main === module) { 
+    console.log( modifiedNames); 
+  }
+
+module.exports = { 
+  testOne, 
+  modifiedNames 
+};
+
+
+
 
